ae45eefa1de969a29eb842ba2f6c65abb0e25c79
Adam Eddy <sr_fallenangel@hotmail.com>
2019-01-15 19:51:31 -0700

    Added PetDBHelper

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,40 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    /** Database file name */
+    private static final String DATABASE_NAME = "shelter.db";
+    /** Database version */
+    private static final int DATABASE_VERSION = 1;
+    /** CREATE table SQL statement */
+    private static final String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+            + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+            + PetEntry.COLUMN_PET_BREED + " TEXT, "
+            + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+            + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+    /** Constructor */
+    public PetDbHelper(Context context) {
+        super(context,DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /** Called when the database is created */
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Execute the SQL statement
+        sqLiteDatabase.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    /** Called when the database version changes */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
